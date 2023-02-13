eed1ade899ae10f260b2709e6008bc6435c82cac
Raj Singh <terahero554@gmail.com>
2023-02-13 19:55:52 +0530

    ....

--- config/database.js
+++ config/database.js
@@ -1,11 +1,29 @@
-const path = require('path');
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
+
+// path: ./config/env/production/database.js
+
+// path: ./config/env/production/database.js
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`, // for a PostgreSQL database
+      // ⚠️ For a MySQL database, use socketPath: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}` instead
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USER'),
+      password: env('DATABASE_PASSWORD'),
     },
-    useNullAsDefault: true,
   },
 });
